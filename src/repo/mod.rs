d1421b0ffa4b7a34d4177648d2e5b3271c4c932e
xgzeng <xgzeng@gmail.com>
2021-12-14 17:28:39 +0800

    improve log

--- src/repo/mod.rs
+++ src/repo/mod.rs
@@ -17,34 +17,26 @@ pub struct RepoManager {
 }
 
 impl RepoMirror {
-    pub fn sync(&self, progress_cb: Option<git2::RemoteCallbacks>) -> Result<()> {
+    pub fn sync(&self, progress_handler: Option<&mut dyn FetchProgressHandler>) -> Result<()> {
         if !self.path.exists() {
             log::info!("{} not exists, sync by clone", self.path.display());
-            self.clone(progress_cb)?;
+            self.clone(progress_handler)?;
         } else {
             log::info!("{} already exists, sync by fetch", self.path.display());
-            self.fetch(progress_cb)?;
+            self.fetch(progress_handler)?;
         }
         Ok(())
     }
 
-    fn clone(&self, progress_cb: Option<git2::RemoteCallbacks>) -> Result<()> {
-        // Proxy Options
-        // let mut proxy_opts = git2::ProxyOptions::new();
-        // build and clone
-        let mut fetch_options = git2::FetchOptions::new();
-        match progress_cb {
-            Some(cb) => {
-                fetch_options.remote_callbacks(cb);
-                ()
-            }
-            _ => (),
+    fn clone(&self, progress_handler: Option<&mut dyn FetchProgressHandler>) -> Result<()> {
+        let mut fetch_opts = git2::FetchOptions::new();
+        if let Some(handler) = progress_handler {
+            fetch_opts.remote_callbacks(handler.as_remote_callbacks());
         }
-        // fetch_options.proxy_options(proxy_opts);
 
         let mut builder = git2::build::RepoBuilder::new();
         builder.bare(true);
-        builder.fetch_options(fetch_options);
+        builder.fetch_options(fetch_opts);
         builder.remote_create(|repo, name, url| {
             // Create remote with a mirror refspec
             let remote = repo.remote_with_fetch(name, url, "+refs/*:refs/*")?;
@@ -59,36 +51,48 @@ impl RepoMirror {
         Ok(())
     }
 
-    fn fetch(&self, progress_cb: Option<git2::RemoteCallbacks>) -> Result<()> {
-        let repo = git2::Repository::open_bare(&self.path)?;
-        let mut remote = repo.find_remote("origin")?;
-        // let mut refspecs = remote.fetch_refspecs()?;
-
-        log::info!("connect {}", remote.url().expect("remote no url"));
-        remote
-            .connect(git2::Direction::Fetch)
-            .context("connect to remote")?;
-
+    fn fetch(&self, progress_handler: Option<&mut dyn FetchProgressHandler>) -> Result<()> {
         let mut fetch_opts = git2::FetchOptions::new();
-        match progress_cb {
-            Some(cb) => {
-                fetch_opts.remote_callbacks(cb);
-                ()
-            }
-            _ => (),
+        if let Some(handler) = progress_handler {
+            fetch_opts.remote_callbacks(handler.as_remote_callbacks());
         }
 
-        log::info!("remote download");
-        let specs: Vec<&str> = vec![];
-        remote.download(&specs, Some(&mut fetch_opts))?;
+        let repo = git2::Repository::open_bare(&self.path)?;
+        let mut remote = repo.find_remote("origin")?;
 
-        remote.update_tips(
-            None, // progress_cb.as_mut(),
-            true,
-            git2::AutotagOption::All,
-            Some("some message"),
-        )?;
-        // remote.fetch(, Some(&mut fetch_options), None)?;
+        // log::info!("connect {}", remote.url().expect("remote no url"));
+        // remote
+        //     .connect(git2::Direction::Fetch)
+        //     .context("connect to remote")?;
+
+        // {
+        //     log::info!("remote download");
+        //     let fetch_opts = git2::FetchOptions::new();
+
+        //     let callbacks: Option<git2::RemoteCallbacks<'_>> =
+        //         progress_handler.map(|h| h.as_remote_callbacks());
+
+        //     if let Some(cb) = callbacks {
+        //         fetch_opts.remote_callbacks(cb);
+        //     }
+
+        //     let specs: Vec<&str> = vec![];
+        //     remote.download(&specs, Some(&mut fetch_opts))?;
+
+        //     // if let Some(handler) = &progress_handler {
+        //     //     handler.on_download_begin();
+        //     // }
+        // }
+
+        // remote.update_tips(
+        //     None, // callbacks1.as_mut(),
+        //     true,
+        //     git2::AutotagOption::All,
+        //     Some("some message"),
+        // )?;
+
+        let refs = ["+refs/*:refs/*"];
+        remote.fetch(&refs, Some(&mut fetch_opts), None)?;
         Ok(())
     }
 }
@@ -106,7 +110,7 @@ impl RepoManager {
     //         repo_list: vec![],
     //     });
     // }
-    fn parse_github_repo_string(repo_str: &String) -> Result<RepoMirror> {
+    fn parse_github_repo_string(repo_str: &str) -> Result<RepoMirror> {
         let names: Vec<&str> = repo_str.split('/').collect();
         match &names[..] {
             [org_name, repo_name] => {
@@ -122,9 +126,9 @@ impl RepoManager {
             }
         }
     }
-    pub fn add_github_repo(&mut self, name: &String) -> Result<()> {
+    pub fn add_github_repo(&mut self, name: &str) -> Result<()> {
         log::info!("add github repository {}", name);
-        if name.contains("/") {
+        if name.contains('/') {
             let repo = Self::parse_github_repo_string(name)?;
             self.repo_list.push(repo);
             Ok(())
@@ -137,11 +141,10 @@ impl RepoManager {
     pub fn update(&self) {
         for r in &self.repo_list {
             let mut progress_handler = ProgressIndicator::new();
-            match r.sync(Some(progress_handler.as_remote_callbacks())) {
-                // to_remote_callbacks(&mut progress_handler)
-                Err(err) => log::error!("update '{}' error: {:?}", r.url, err),
-                _ => (),
+            if let Err(err) = r.sync(Some(&mut progress_handler)) {
+                log::error!("update '{}' error: {:?}", r.url, err);
             }
+            progress_handler.hide();
         }
     }
 }
--- src/repo/progress.rs
+++ src/repo/progress.rs
@@ -4,10 +4,11 @@ use std::rc::Rc;
 
 pub trait FetchProgressHandler {
     fn on_transfer(&mut self, p: git2::Progress);
+
     fn on_update_tips(&mut self, name: &str, oid_from: git2::Oid, oid_to: git2::Oid);
     fn on_sideband(&mut self, msg: &[u8]);
 
-    fn as_remote_callbacks<'a>(&'a mut self) -> git2::RemoteCallbacks<'a> {
+    fn as_remote_callbacks(&mut self) -> git2::RemoteCallbacks<'_> {
         let mut callbacks = git2::RemoteCallbacks::new();
 
         let rc_handler = Rc::new(RefCell::new(self));
@@ -37,38 +38,58 @@ pub trait FetchProgressHandler {
     }
 }
 
-pub struct LogFetchProgress {}
+// pub struct LogFetchProgress {}
 
-impl FetchProgressHandler for LogFetchProgress {
-    fn on_transfer(&mut self, p: git2::Progress) {
-        log::info!(
-            "objects: total {}, received {},",
-            p.total_objects(),
-            p.received_objects()
-        );
-    }
+// impl FetchProgressHandler for LogFetchProgress {
+//     fn on_transfer(&mut self, p: git2::Progress) {
+//         log::info!(
+//             "objects: total {}, received {},",
+//             p.total_objects(),
+//             p.received_objects()
+//         );
+//     }
 
-    fn on_update_tips(&mut self, name: &str, oid_from: git2::Oid, oid_to: git2::Oid) {
-        log::info!("update_tips: {} {} {}", name, oid_from, oid_to);
-    }
+//     fn on_update_tips(&mut self, name: &str, oid_from: git2::Oid, oid_to: git2::Oid) {
+//         log::info!("update_tips: {} {} {}", name, oid_from, oid_to);
+//     }
 
-    fn on_sideband(&mut self, msg: &[u8]) {
-        log::info!("sideband_progress: {}", String::from_utf8_lossy(msg));
-    }
+//     fn on_sideband(&mut self, msg: &[u8]) {
+//         log::info!("sideband_progress: {}", String::from_utf8_lossy(msg));
+//     }
+// }
+
+enum ProgressStage {
+    NotStarted,
+    Download,
+    Sideband,
 }
 
 pub struct ProgressIndicator {
     indicator: ProgressBar,
+
+    stage: ProgressStage,
 }
 
 impl FetchProgressHandler for ProgressIndicator {
     fn on_transfer(&mut self, p: git2::Progress) {
+        match self.stage {
+            ProgressStage::Download => (),
+            _ => {
+                self.stage = ProgressStage::Download;
+                self.indicator
+                    .set_style(ProgressStyle::default_bar().template(
+                        "[{elapsed_precise}] {bar:40.cyan/blue} Downloading {pos:>7}/{len:7}",
+                    ));
+            }
+        }
+
         let total = p.total_objects() as u64;
+        let received = p.received_objects() as u64;
+
         if total != self.indicator.length() {
             self.indicator.set_length(total);
         }
 
-        let received = p.received_objects() as u64;
         self.indicator.set_position(received);
     }
 
@@ -77,7 +98,17 @@ impl FetchProgressHandler for ProgressIndicator {
     }
 
     fn on_sideband(&mut self, bytes: &[u8]) {
+        match self.stage {
+            ProgressStage::Sideband => (),
+            _ => {
+                self.stage = ProgressStage::Sideband;
+                self.indicator
+                    .set_style(ProgressStyle::default_bar().template("[{elapsed_precise}] {msg}"));
+            }
+        }
+
         let msg = String::from_utf8_lossy(bytes);
+        // log::info!("remote: {}", msg);
         self.indicator.set_message(msg.into_owned());
         self.indicator.tick();
     }
@@ -90,6 +121,13 @@ impl ProgressIndicator {
             ProgressStyle::default_bar()
                 .template("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}"),
         );
-        ProgressIndicator { indicator: ind }
+        ProgressIndicator {
+            indicator: ind,
+            stage: ProgressStage::NotStarted,
+        }
+    }
+
+    pub fn hide(&mut self) {
+        self.indicator.finish_and_clear();
     }
 }
