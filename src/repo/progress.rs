75e599f877ff4078dee4afabea87b682461b951b
XiaoGuang Zeng <xgzeng@gmail.com>
2021-12-21 11:32:08 +0800

    improve progress indicator

--- src/repo/progress.rs
+++ src/repo/progress.rs
@@ -1,6 +1,7 @@
 use indicatif::{ProgressBar, ProgressStyle};
 use std::cell::RefCell;
 use std::rc::Rc;
+use std::time::Instant;
 
 pub trait FetchProgressHandler {
     fn on_transfer(&mut self, p: git2::Progress);
@@ -60,37 +61,48 @@ pub trait FetchProgressHandler {
 
 enum ProgressStage {
     NotStarted,
-    Download,
+    Download(u64, Instant), // received bytes, receive_time
     Sideband,
 }
 
 pub struct ProgressIndicator {
     indicator: ProgressBar,
-
     stage: ProgressStage,
 }
 
 impl FetchProgressHandler for ProgressIndicator {
     fn on_transfer(&mut self, p: git2::Progress) {
         match self.stage {
-            ProgressStage::Download => (),
+            ProgressStage::Download(prev_received_bytes, prev_recv_time) => {
+                let received = p.received_objects() as u64;
+                let new_recv_time = Instant::now();
+                let duration = new_recv_time.duration_since(prev_recv_time).as_secs();
+                if duration != 0 {
+                    let datarate = received - prev_received_bytes;
+                    self.stage = ProgressStage::Download(received, new_recv_time);
+                    self.indicator.set_message(format!("{}B/s", datarate));
+                }
+                self.indicator.set_position(received);
+            }
             _ => {
-                self.stage = ProgressStage::Download;
+                // enter download state
+                self.reset(); // recreate indicator
+                              // self.indicator.reset();
+                              // self.indicator.finish_and_clear(); // clear indicator
+                log::info!(
+                    "download objects: total {}, total_deltas {}, local {}",
+                    p.total_objects(),
+                    p.total_deltas(),
+                    p.local_objects()
+                );
+                self.stage = ProgressStage::Download(0, Instant::now());
+                self.indicator.set_length(p.total_objects() as u64);
                 self.indicator
                     .set_style(ProgressStyle::default_bar().template(
-                        "[{elapsed_precise}] {bar:40.cyan/blue} Downloading {pos:>7}/{len:7}",
+                        "[{elapsed_precise}] {bar:40.cyan/blue} Objects {pos:>7}/{len:7}, {msg}",
                     ));
             }
         }
-
-        let total = p.total_objects() as u64;
-        let received = p.received_objects() as u64;
-
-        if total != self.indicator.length() {
-            self.indicator.set_length(total);
-        }
-
-        self.indicator.set_position(received);
     }
 
     fn on_update_tips(&mut self, name: &str, oid_from: git2::Oid, oid_to: git2::Oid) {
@@ -127,7 +139,11 @@ impl ProgressIndicator {
         }
     }
 
-    pub fn hide(&mut self) {
-        self.indicator.finish_and_clear();
+    fn reset(&mut self) {
+        self.indicator = ProgressBar::new(100);
     }
+
+    // pub fn hide(&mut self) {
+    //     self.indicator.finish_and_clear();
+    // }
 }
